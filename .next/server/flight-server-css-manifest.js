a353feb31dbb2483e0d416a6aa453ff9b82a13b7
Greatherdan <danielchr93@gmail.com>
2023-02-05 21:28:57 +0000

    Update 72 files

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_CSS_MANIFEST={"__entry_css__":{"/project/starkentreprenor/app/page":["/project/starkentreprenor/styles/globals.css"]},"/project/starkentreprenor/app/layout.js":["/project/starkentreprenor/styles/globals.css"]}
